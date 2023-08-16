efd23a8277e1ba8f668b28cfb17434e4da794a65
zhuhongji <958157389@qq.com>
2023-08-16 18:04:19 +0800

    初版功能确定

---
+++ js/diary/diary.js
@@ -0,0 +1,3 @@
+$('.2023-08-16').on('click', () => {
+  window.location.href = '../../html/diary/diaryList/diary230816.html';
+});
---
+++ js/index/index.js
@@ -0,0 +1,12 @@
+$('.goJeecg').on('click', () => {
+  window.location.href = './html/jeecg-vue2/jeecgVue2.html';
+});
+
+$('.goTxt').on('click', () => {
+  window.location.href = './html/novel/novel.html';
+});
+
+$('.goDiary').on('click', () => {
+  window.location.href = './html/diary/diary.html';
+});
+
